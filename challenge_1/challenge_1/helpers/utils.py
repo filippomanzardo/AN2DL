bdbc4c2b934a5d2bcfb62254a31469270acbd9f9
Filippo Manzardo <film@bendingspoons.com>
2022-11-08 21:12:16 +0100

    feat: first commit

---
+++ challenge_1/challenge_1/helpers/utils.py
@@ -0,0 +1,53 @@
+import json
+import logging
+import shutil
+import uuid
+from datetime import datetime
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Any
+import inspect
+from challenge_1.models.base_model import TrainableModel
+
+_LOGGER = logging.getLogger(__name__)
+
+_METADATA_FILE_CONTENT = "42"
+_MODEL_FILE_CONTENT = """
+import os
+import tensorflow as tf
+class Model:
+    def __init__(self, path):
+        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel'))
+    
+"""
+
+def prepare_submission(model: Model, save_path: Path) -> None:
+    """
+    Prepare the submission for the challenge.
+
+    :param model:  the model to be submitted
+    :param save_path: the path where the submission should be saved
+    """
+    metadata = _generate_metadata(model.stats)
+    out_dir = save_path / ("submission_" + datetime.now().strftime("%Y%m%d_%H%M%S"))
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    _LOGGER.info("⚙️ Saving model to %s ⚙️", out_dir)
+    with TemporaryDirectory() as tmpdir:
+        model.save(Path(tmpdir) / "SubmissionModel")
+        (Path(tmpdir) / "metadata").write_text(_METADATA_FILE_CONTENT)
+        (Path(tmpdir) / "model.py").write_text(_MODEL_FILE_CONTENT + inspect.getsource(model.predict))
+
+        shutil.make_archive((out_dir / "submission").as_posix(), "zip", tmpdir)
+
+    (Path(out_dir) / "metadata.json").write_text(json.dumps(metadata))
+
+    _LOGGER.info("✅ Submission ID: %s, saved to %s ✅", metadata["id"], out_dir)
+
+
+def _generate_metadata(stats: dict[str, Any]) -> dict[str, Any]:
+    return {
+        "id": str(int(uuid.uuid4()))[:8],
+        "exported_at": datetime.now().isoformat(),
+        **stats,
+    }
---
+++ challenge_1/challenge_1/models/base_model.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from typing import Any
+import abc
+import tensorflow as tf
+
+
+class TrainableModel(abc.ABC):
+    """A trainable model metaclass."""
+
+    @abc.abstractmethod
+    @property
+    def model(self) -> str:
+        """Return the model of this instance."""
+        pass
+
+    @abc.abstractmethod
+    @property
+    def model_file(self) -> str:
+        """Return the model file as a string."""
+        pass
+
+    @abc.abstractmethod
+    @property
+    def stats(self) -> dict[str, Any]:
+        """Return the stats of the model."""
+        pass
+
+    @abc.abstractmethod
+    def save(self, path: Path) -> None:
+        """Save the model to the given path."""
+        pass
+
+    @abc.abstractmethod
+    def train(self, X: Any, y: Any) -> None:
+        """Train the model."""
+        pass
+
+    @abc.abstractmethod
+    def predict(self, X: Any) -> Any:
+        """Predict the output for the given input."""
+        pass
+
+
