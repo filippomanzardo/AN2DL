5001f77dba4d322a0ac5eb2c3c2247d7e531e776
Filippo Manzardo <film@bendingspoons.com>
2022-11-09 21:27:59 +0100

    feat: copilot model

--- challenge_1/challenge_1/main.py
+++ challenge_1/challenge_1/main.py
@@ -1,5 +1,72 @@
+from pathlib import Path
+
+import tensorflow as tf
+
+from challenge_1.helpers.utils import prepare_submission
+from challenge_1.models.base_model import CopilotModel
+
+_DATASET_DIRECTORY = Path(".") / "dataset"
+_SEED = 0
+
+
 def main() -> None:
-    print("Hello, world!")
+    generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1.0 / 255, validation_split=0.25
+    )
+
+    training_dataset = generator.flow_from_directory(
+        directory=_DATASET_DIRECTORY,
+        target_size=(96, 96),
+        color_mode="rgb",
+        seed=_SEED,
+        subset="training",
+        save_format="jpg",
+    )
+
+    validation_dataset = generator.flow_from_directory(
+        directory=_DATASET_DIRECTORY,
+        target_size=(96, 96),
+        color_mode="rgb",
+        seed=_SEED,
+        subset="validation",
+        save_format="jpg",
+    )
+
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Conv2D(
+                filters=32,
+                kernel_size=(3, 3),
+                activation="relu",
+                input_shape=(96, 96, 3),
+            ),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(units=128, activation="relu"),
+            tf.keras.layers.Dense(units=8, activation="softmax"),
+        ]
+    )
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.CategoricalAccuracy()],
+    )
+
+    model.fit(
+        x=training_dataset,
+        validation_data=validation_dataset,
+        epochs=10,
+        verbose=1,
+    )
+
+    my_model = CopilotModel(model)
+
+    prepare_submission(my_model, Path("submissions"))
 
 
 if __name__ == "__main__":
--- challenge_1/challenge_1/models/base_model.py
+++ challenge_1/challenge_1/models/base_model.py
@@ -2,19 +2,21 @@ import abc
 from pathlib import Path
 from typing import Any
 
+import tensorflow as tf
+
 
 class TrainableModel(abc.ABC):
-    """A trainable model metaclass."""
+    """An abstract trainable model."""
 
-    @abc.abstractmethod
     @property
-    def model(self) -> str:
+    def model(self) -> tf.keras.models.Model:
         """Return the model of this instance."""
+        raise NotImplementedError
 
-    @abc.abstractmethod
     @property
     def stats(self) -> dict[str, Any]:
         """Return the stats of the model."""
+        raise NotImplementedError
 
     @abc.abstractmethod
     def save(self, path: Path) -> None:
@@ -27,3 +29,33 @@ class TrainableModel(abc.ABC):
     @abc.abstractmethod
     def predict(self, X: Any) -> Any:
         """Predict the output for the given input."""
+
+
+class CopilotModel(TrainableModel):
+    """A model that can be trained and used to predict."""
+
+    def __init__(self, model: tf.keras.models.Model) -> None:
+        self._model = model
+
+    @property
+    def model(self) -> tf.keras.models.Model:
+        """Return the model of this instance."""
+        return self._model
+
+    @property
+    def stats(self) -> dict[str, Any]:
+        """Return the stats of the model."""
+        return {"model": "test"}
+
+    def save(self, path: Path) -> None:
+        """Save the model to the given path."""
+        self._model.save(path)
+
+    def train(self, X: Any, y: Any) -> None:
+        """Train the model."""
+        raise NotImplementedError
+
+    def predict(self, X: Any) -> Any:
+        """Predict the output for the given input."""
+
+        return self._model.predict(X)
